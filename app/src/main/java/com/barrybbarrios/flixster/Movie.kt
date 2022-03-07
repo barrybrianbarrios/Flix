c023b11189750e8a104b379d7a5aa9fb307230cb
barrybrianbarrios <barry.brian.barrios@gmail.com>
2022-03-06 21:57:57 -0600

    Unit 2 added youtube and star rating functionality

--- app/src/main/java/com/barrybbarrios/flixster/Movie.kt
+++ app/src/main/java/com/barrybbarrios/flixster/Movie.kt
@@ -1,14 +1,21 @@
 package com.barrybbarrios.flixster
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
+@Parcelize
 data class Movie (
     val movieId: Int,
+    val VoteAverage: Double,
     private val posterPath: String,
     val title: String,
     val overview: String,
-){
+): Parcelable {
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+
     companion object {
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
         val movies = mutableListOf<Movie>()
@@ -16,6 +23,7 @@ data class Movie (
                 val movieJson = movieJsonArray.getJSONObject(i)
                 movies.add(Movie(
                     movieJson.getInt("id"),
+                    movieJson.getDouble("vote_average"),
                     movieJson.getString("poster_path"),
                     movieJson.getString("title"),
                     movieJson.getString("overview")
